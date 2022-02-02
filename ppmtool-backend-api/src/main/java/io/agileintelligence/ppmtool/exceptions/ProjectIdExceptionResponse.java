e7b758c2e1281a3622e84d7e0ce8980ae544493b
Wayne Chan <wchan.it@outlook.com>
2022-02-02 11:15:37 -0600

    Add custom exceptions for unique project identifiers

---
+++ ppmtool-backend-api/src/main/java/io/agileintelligence/ppmtool/exceptions/ProjectIdExceptionResponse.java
@@ -0,0 +1,11 @@
+package io.agileintelligence.ppmtool.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProjectIdExceptionResponse {
+
+    private String projectIdentifier;
+}
--- ppmtool-backend-api/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ ppmtool-backend-api/src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.agileintelligence.ppmtool.services;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception ex) {
+            throw new ProjectIdException(String.format("Project ID %s already exists", project.getProjectIdentifier()));
+        }
     }
 }
