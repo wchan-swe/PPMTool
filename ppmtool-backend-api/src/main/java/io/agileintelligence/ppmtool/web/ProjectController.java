786644ba196ee59bf7c18afbbba721639f930c80
Wayne Chan <wchan.it@outlook.com>
2022-02-02 02:01:30 -0600

    Project Controller and create first project

---
+++ ppmtool-backend-api/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -0,0 +1,24 @@
+package io.agileintelligence.ppmtool.web;
+
+import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping("/project")
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+        return new ResponseEntity<Project>(projectService.saveOrUpdateProject(project), HttpStatus.CREATED);
+    }
+}
