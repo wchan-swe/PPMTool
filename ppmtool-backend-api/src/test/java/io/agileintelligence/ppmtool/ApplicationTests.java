0c47e7a83a8b76e651e13a89877b42753a2e3769
Wayne Chan <wchan.it@outlook.com>
2022-02-01 23:20:42 -0600

    initial commit

---
+++ ppmtool-backend-api/src/test/java/io/agileintelligence/ppmtool/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
